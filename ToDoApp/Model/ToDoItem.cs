5d6001734ca4b03dfa42ca9166a60e6467463e1f
Tomasz Zacharski <tomasz.zacharski@gmail.com>
2024-10-22 16:01:17 +0200

    Dodaj pliki projektów.

---
+++ ToDoApp/Model/ToDoItem.cs
@@ -0,0 +1,21 @@
+﻿namespace ToDoApp.Model
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        private bool _isCompleted;
+        public bool IsCompleted { get => _isCompleted;
+            set { 
+                _isCompleted = value;
+
+                if (value)
+                {
+                    DateCompleted = DateTime.Now;
+                }
+                }
+            }
+
+        public DateTime DateCompleted { get; set; }
+    }
+}
